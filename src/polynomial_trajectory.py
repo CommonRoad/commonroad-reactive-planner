30bf668bf69b3298b5509ca73b00597366363404
Fabian Konstantinidis <Fabian.Konstantinidis@tum.de>
2019-07-04 22:50:12 +0200

    Initial commit

--- src/polynomial_trajectory.py
+++ src/polynomial_trajectory.py
@@ -44,6 +44,16 @@ class PolynomialTrajectory(ABC):
             self.__cost = cost
 
     def set_cost(self, jerk_cost, time_cost, distance_cost, k_jerk, k_time, k_distance):
+        """
+        Sets and calculates the cost from given weights and cost of jerk, time and distance
+        :param jerk_cost: jerk cost
+        :param time_cost: time cost
+        :param distance_cost: distance cost
+        :param k_jerk: jerk weight
+        :param k_time: time weight
+        :param k_distance: distance weight
+        :return: none
+        """
         self.cost = k_jerk * jerk_cost + k_time * time_cost + k_distance * distance_cost
 
     def squared_jerk_integral(self, t):
@@ -66,9 +76,19 @@ class PolynomialTrajectory(ABC):
         return integral_squared_jerk
 
     def get_velocity_cost(self, desired_velocity):
+        """
+        Calculates velocity cost for reaching desired_velocity
+        :param desired_velocity: desired velocity
+        :return: velocity cost
+        """
         return (self.calc_velocity_at(self.duration_s) - desired_velocity)**2
 
     def get_distance_cost(self, desired_distance):
+        """
+        Calculates velocity cost for reaching desired_velocity
+        :param desired_distance: desired distance
+        :return: distance cost
+        """
         return (self.calc_position_at(self.duration_s) - desired_distance)**2
 
     # evaluate polynomials
@@ -101,16 +121,33 @@ class PolynomialTrajectory(ABC):
         return np.array([p, p_d, p_dd])
 
     def calc_jerk_at(self, t):
+        """
+        Calculates jerk at time t
+        :param t: time
+        :return: jerk
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
         return 6 * self.coeffs[3] + 24 * self.coeffs[4] * t + 60 * self.coeffs[5] * t * t
 
     def calc_acceleration(self, t,t2,t3):
+        """
+        Calculates acceleration at time t
+        :param t: time t
+        :param t2: t * t
+        :param t3: t * t * t
+        :return: acceleration
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
         return 2 * self.coeffs[2] + 6 * self.coeffs[3] * t + 12 * self.coeffs[4] * t2 + 20 * self.coeffs[5] * t3
 
     def calc_acceleration_at(self, t):
+        """
+        Calculates acceleration
+        :param t: time
+        :return: acceleration
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
         t2 = t * t
@@ -118,12 +155,25 @@ class PolynomialTrajectory(ABC):
         return 2 * self.coeffs[2] + 6 * self.coeffs[3] * t + 12 * self.coeffs[4] * t2 + 20 * self.coeffs[5] * t3
 
     def calc_velocity(self, t,t2,t3,t4):
+        """
+        Calculates velocity
+        :param t: time t
+        :param t2: t * t
+        :param t3: t * t * t
+        :param t4: t * t * t * t
+        :return: velocity
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
         return (self.coeffs[1] + 2. * self.coeffs[2] * t + 3. * self.coeffs[3] * t2 + 4. * self.coeffs[4] * t3 +
                 5. * self.coeffs[5] * t4)
 
     def calc_velocity_at(self, t):
+        """
+        Calculates velocity at time t
+        :param t: time
+        :return: velocity
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
         t2 = t * t
@@ -133,12 +183,26 @@ class PolynomialTrajectory(ABC):
                 5. * self.coeffs[5] * t4)
 
     def calc_position(self, t,t2,t3,t4,t5):
+        """
+        Calculates position
+        :param t: time t
+        :param t2: t * t
+        :param t3: t * t * t
+        :param t4: t * t * t * t
+        :param t5: t * t * t * t * t
+        :return: position
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
         return (self.coeffs[0] + self.coeffs[1] * t + self.coeffs[2] * t2 + self.coeffs[3] * t3 +
                 self.coeffs[4] * t4 + self.coeffs[5] * t5)
 
     def calc_position_at(self, t):
+        """
+        Calculates position at time t
+        :param t:
+        :return: position
+        """
         if self.coeffs is None:
             raise ValueError('Coefficients are not determined')
 
@@ -156,6 +220,10 @@ class QuinticTrajectory(PolynomialTrajectory):
                                                 start_state=start_state, end_state=end_state)
 
     def calc_coeffs(self):
+        """
+        calculates coefficients of quintic trajectory for given start state, end state and desired duration
+        :return:
+        """
         p_init = self.start_state[0]
         p_init_d = self.start_state[1]
         p_init_dd = self.start_state[2]
@@ -183,6 +251,7 @@ class QuinticTrajectory(PolynomialTrajectory):
             print(e)
             return None
         return np.array([p_init, p_init_d, .5 * p_init_dd, x[0], x[1], x[2]])
+
 
 
 class QuarticTrajectory(PolynomialTrajectory):
--- src/trajectory_bundle.py
+++ src/trajectory_bundle.py
@@ -1,65 +1,6 @@
 from typing import Union
 import numpy as np
 
-# planning parameter
-class PlanningParameter:
-    k_long = None
-    k_lat = None
-
-    k_jerk_lon = None
-    k_jerk_lat = None
-    k_time = None
-    k_distance = None
-
-    prediction_horizon = None
-    speed_limit = None
-
-    t_step_size = None
-    lat_step_size = None
-    long_step_size = None
-
-
-# vehicle parameter
-class VehicleParameter:
-    jerk_long_max = None
-    jerk_lat_max = None
-
-    acceleration_max = 8  # m/s²
-    velocity_max = 150 / 3.6  # m/s
-
-    curvature_max = 0.2
-
-    width = 1.674  # vehicle width [m]
-    length = 4.298  # vehicle length [m]
-
-    # parameters for calculating steering angle
-
-    length_front = length / 3
-    length_rear = 2 * length / 3
-    stiffness_front = 40000  # front tire stiffness [N/rad]
-    stiffness_rear = 40000  # rear tire stiffens [N/rad]
-    mass = 1500  # mass of vehicle [kg]
-
-def parameter_velocity_reaching():
-    params = PlanningParameter()
-
-    params.k_long = .1
-    params.k_lat = 5.0
-
-    params.k_jerk_lat = 5.0
-    params.k_jerk_lon = 5.0
-    params.k_time = 10.0
-    params.k_distance = 2.0
-
-    params.prediction_horizon = 3.0  # s
-    params.speed_limit = 130.0 / 3.6  # m/s
-
-    params.t_step_size = .5  # s
-    params.lat_step_size = 1
-    params.long_step_size = 1
-
-    return params
-
 
 class CartesianSample:
     def __init__(self, x, y, theta, v, a, kappa, kappa_dot):
@@ -94,7 +35,10 @@ class TrajectorySample:
         self.ext_curvilinear = None
 
     def reevaluate_costs(self):
-        desired_time = self.trajectory_long.desired_horizon
+        """ Calculates the cost for sampled trajectory
+        :return: calculated cost
+        """
+        # desired_time = self.trajectory_long.desired_horizon
         desired_speed = self.trajectory_long._desired_velocity
 
         a_u = np.append(self.cartesian.a,self.ext_cartesian.a) if self.ext_cartesian is not None else self.cartesian.a
@@ -103,7 +47,7 @@ class TrajectorySample:
         theta_u = np.append(self.curvilinear.theta,self.ext_curvilinear.theta) if self.ext_cartesian is not None else self.curvilinear.theta
 
         # acceleration costs
-        costs = np.sum((1*a_u) ** 2)
+        costs = np.sum(a_u ** 2)
         # velocity costs
         costs += np.sum((5*(v_u - desired_speed))**2)
         # distance costs
@@ -119,23 +63,32 @@ class TrajectorySample:
 
 
 class TrajectoryBundle:
-    def __init__(self, params: PlanningParameter):
+    def __init__(self):
         self.trajectory_bundle = []
-        self.params = params
-
-    def optimal_trajectory(self) -> Union[TrajectorySample, None]:
-        """
-        :return: trajectory in trajectory_bundle with minimal cost. None, if trajectory bundle is empty.
-        """
-        if not self.trajectory_bundle:
-            return None
-        return min(self.trajectory_bundle, key=lambda x: x.total_cost)
 
     def min_costs(self) -> float:
+        """ Computes minimal cost for all sampled trajectories in trajectory bundle
+        :return: minimal cost. None, if trajectory bundle is empty.
+        """
         return min([x.total_cost for x in self.trajectory_bundle])
 
     def max_costs(self) -> float:
+        """ Computes max cost for all sampled trajectories in trajectory bundle
+        :return: maximal cost. None, if trajectory bundle is empty.
+        """
         return max([x.total_cost for x in self.trajectory_bundle])
+
+    def add_trajectory(self, trajectory: TrajectorySample):
+        """ Add trajectory to trajectory bundle list
+        :param: trajectory: new trajectory to add
+        """
+        self.trajectory_bundle.append(trajectory)
+
+    def empty(self) -> bool:
+        """ Check if trajectory bundle list is empty
+        :return: true if no trajectories are stored in trajectory bundle else false
+        """
+        return len(self.trajectory_bundle) == 0
 
     def updated_optimal_trajectory(self) -> Union[TrajectorySample, None]:
         """
@@ -145,19 +98,25 @@ class TrajectoryBundle:
             return None
         return min(self.trajectory_bundle, key=lambda x: x.reevaluate_costs())
 
-    def optimal_trajectory_idx(self) -> Union[int, None]:
-        """
-        :return: index of trajectory in trajectory_bundle with minimal cost. None, if trajectory bundle is empty.
-        """
-        if not self.trajectory_bundle:
-            return None
-        return self.trajectory_bundle.index(min(self.trajectory_bundle, key=lambda x: x.total_cost))
-
-    def add_trajectory(self, trajectory: TrajectorySample):
-        self.trajectory_bundle.append(trajectory)
-
-    def remove_trajectory(self, trajectory_idx: int):
-        self.trajectory_bundle.pop(trajectory_idx)
-
-    def empty(self) -> bool:
-        return len(self.trajectory_bundle) == 0
+    #def optimal_trajectory(self) -> Union[TrajectorySample, None]:
+    #    """
+    #    :return: trajectory in trajectory_bundle with minimal cost. None, if trajectory bundle is empty.
+    #    """
+    #    if not self.trajectory_bundle:
+    #        return None
+    #    return min(self.trajectory_bundle, key=lambda x: x.total_cost)
+    #
+    #def remove_trajectory(self, trajectory_idx: int):
+    #    """ Remove trajectory to trajectory bundle list
+    #    :param: trajectory_idx: id of trajectory to remove
+    #    """
+    #    self.trajectory_bundle.pop(trajectory_idx)
+#
+    #def optimal_trajectory_idx(self) -> Union[int, None]:
+    #    """
+    #    :return: index of trajectory in trajectory_bundle with minimal cost. None, if trajectory bundle is empty.
+    #    """
+    #    if not self.trajectory_bundle:
+    #        return None
+    #    return self.trajectory_bundle.index(min(self.trajectory_bundle, key=lambda x: x.total_cost))
+#
