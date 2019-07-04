66a232d0a9406811d6b39e4ce864f16dff11063b
Julian Ost <julian.ost@outlook.de>
2019-07-04 11:26:18 +0200

    Improved Scenario Loading

--- src/main.py
+++ src/main.py
@@ -13,21 +13,34 @@ from parameter import PlanningParameter, VehicleParameter
 if __name__ == '__main__':
     print('Creating velocity reaching bundle....')
 
-    scenario_path = '/home/julian/Desktop/commonroadlibrary/commonroad-scenarios/hand-crafted/ZAM_Over-1_1.xml'
+    file_path = '/home/julian/Desktop/commonroadlibrary/commonroad-scenarios/hand-crafted/ZAM_Over-1_1.xml'
 
-    crfr = CommonRoadFileReader(scenario_path)
-    scenario, _ = crfr.open()
+    crfr = CommonRoadFileReader(file_path)
+    scenario, PlanningProblemSet = crfr.open()
     plt.figure(figsize=(25, 10))
     draw_object(scenario)
     plt.axis('equal')
     plt.show(block=False)
     plt.pause(0.1)
-    scenario = ReactivePlanner.add_obstacles_at_lanelet_edges(ReactivePlanner, scenario, scenario_path)
+    scenario = ReactivePlanner.add_obstacles_at_lanelet_edges(ReactivePlanner, scenario, file_path)
 
-    root = ET.parse(scenario_path).getroot()
-    for lane in root.findall('lanelet'):
-        laneletid= lane.get('id')
-        break
+    if not PlanningProblemSet:
+        root = ET.parse(file_path).getroot()
+        for lane in root.findall('lanelet'):
+            laneletid = lane.get('id')
+            break
+        print('No Planning Problem specified for this scenario!')
+    else:
+        root = ET.parse(file_path).getroot()
+        for problem in root.findall('planningProblem'):
+            problemid = problem.get('id')
+            break
+        init_state = PlanningProblemSet.find_planning_problem_by_id(int(problemid)).initial_state
+        x, y = init_state.position
+        heading = init_state.orientation
+        x_1, y_1 = np.array([x, y]) + np.array([1, 1 * np.tan(heading)])
+        tmp = np.array([[x, y], [x_1, y_1]])
+        laneletid = scenario.lanelet_network.find_lanelet_by_position(tmp)[0][0]
 
     # create coordinate system
     reference_path = scenario.lanelet_network.find_lanelet_by_id(int(laneletid)).center_vertices
@@ -36,9 +49,17 @@ if __name__ == '__main__':
     # create collision checker for scenario
     collision_checker = create_collision_checker(scenario)
 
-    # convert coordinates and create initial state
-    x, y = curvilinear_cosy.convert_to_cartesian_coords(25, 0)
-    x_0 = State(**{'position':np.array([x, y]),'orientation':0.04, 'velocity':10, 'acceleration':0,'yaw_rate':0})
+    # create initial state
+    if not PlanningProblemSet:
+        i_init = 2
+        x, y = reference_path[i_init, :]
+        x_1, y_1 = reference_path[i_init + 1, :]
+        heading = np.arctan2(x_1 - x, y_1 - y)
+    else:
+        heading = init_state.orientation
+
+    x_0 = State(**{'position': np.array([x, y]), 'orientation': heading, 'velocity': 10, 'acceleration': 0, 'yaw_rate': 0})
+    print(x_0.position)
 
     # Set planning parameters and vehicle parameters
     params_planning = PlanningParameter(velocity_reaching=True)
@@ -51,7 +72,7 @@ if __name__ == '__main__':
 
     x_cl = None
 
-    for k in range(0, 100):
+    for k in range(0, 50):
         optimal = planner.plan(x_0, collision_checker, cl_states=x_cl)
         # convert to CR obstacle
         ego = planner.convert_cr_trajectory_to_object(optimal[0])
