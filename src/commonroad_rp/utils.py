Fixed bug in orientation computation

src/commonroad_rp/utils.py
@@ -25,8 +25,14 @@ def compute_orientation_from_polyline(polyline: np.ndarray) -> np.ndarray:
     if (len(polyline) < 2):
         raise NameError('Cannot create orientation from polyline of length < 2')
 
-    orientation = [0]
-    for i in range(1, len(polyline)):
+    orientation = []
+    for i in range(0, len(polyline) - 1):
+        pt1 = polyline[i]
+        pt2 = polyline[i + 1]
+        tmp = pt2 - pt1
+        orientation.append(np.arctan2(tmp[1], tmp[0]))
+
+    for i in range(len(polyline) - 1, len(polyline)):
         pt1 = polyline[i - 1]
         pt2 = polyline[i]
         tmp = pt2 - pt1